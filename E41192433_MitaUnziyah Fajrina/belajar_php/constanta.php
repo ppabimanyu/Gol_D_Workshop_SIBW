414853d40b0f478d7174d0887aeb8325de46f39a
Mita Unziyah Fajrina <arinmita08@gmail.com>
2020-10-27 20:25:50 +0700

    TugasMinggu6

---
+++ E41192433_MitaUnziyah Fajrina/belajar_php/constanta.php
@@ -0,0 +1,13 @@
+<?php // file: belajar-konstanta.php
+
+// membuat konstanta
+define('VERSION', '1.0.0');
+
+const SITE_NAME = "Petanikode";
+const BASE_URL = "http://www.petanikode.com";
+
+// cetak nilai konstanta
+echo "Site name: " . SITE_NAME . "<br/>";
+echo "Url: " . BASE_URL . "<br/>";
+echo "Version: " . VERSION . "<br/>";
+?>
