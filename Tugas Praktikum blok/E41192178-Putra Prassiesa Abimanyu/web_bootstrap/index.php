98e04bdb0248103a10bbc269c33021d6175cf5a4
Putra-Prassiesa-Abimanyu <ppabimanyu@gmail.com>
2020-11-19 15:02:08 +0700

    Tugas Praktikum Blok

--- Tugas Praktikum blok/E41192178-Putra Prassiesa Abimanyu/web_bootstrap/index.php
+++ Tugas Praktikum blok/E41192178-Putra Prassiesa Abimanyu/web_bootstrap/index.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+if (!isset($_SESSION['email'])) {
+  header("location: login.php");
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -566,7 +572,7 @@
         <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
         <div class="modal-footer">
           <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-          <a class="btn btn-primary" href="login.html">Logout</a>
+          <a class="btn btn-primary" href="action-logout.php">Logout</a>
         </div>
       </div>
     </div>
--- Tugas Praktikum blok/E41192178-Putra Prassiesa Abimanyu/web_bootstrap/login_process.php
+++ Tugas Praktikum blok/E41192178-Putra Prassiesa Abimanyu/web_bootstrap/login_process.php
@@ -4,7 +4,10 @@ $password = $_POST['password'];
 $email_saya = "ppabimanyu@gmail.com";
 $password_saya = 12345678;
 if((strcasecmp($email_saya, $email) == 0)&&($password_saya==$password))
-    {header("location:index.php?pesan=berhasil");}
+{
+session_start();
+$_SESSION['email'] = $email;
+    header("location:index.php?pesan=berhasil");}
 else
     {header("location:login.php?pesan=gagal");}
 ?>
